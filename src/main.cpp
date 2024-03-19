be011995b34e25ffe20f29be2330d864b7274710
JosepUPC <github.jeb@gmail.com>
2024-03-19 12:59:17 +0100

    Master

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+#include <wire.h>
+#include <AHT10.h>
+
+#define SDA 21
+#define SDI 20
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
