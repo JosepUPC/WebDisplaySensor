2d84872d4a05016ff303ee498790d5e952396280
JosepUPC <github.jeb@gmail.com>
2024-03-19 20:07:26 +0100

    Correction

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,47 @@
 #include <Arduino.h>
-#include <wire.h>
-#include <AHT10.h>
-
-#define SDA 21
-#define SDI 20
-
-void setup() {
-  Serial.begin(115200);
+#include <Wire.h>
+void setup()
+{
+Wire.begin();
+Serial.begin(115200);
+while (!Serial);
+// Leonardo: wait for serial monitor
+Serial.println("\nI2C Scanner");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
+byte error, address;
+int nDevices;
+Serial.println("Scanning...");
+nDevices = 0;
+for(address = 1; address < 127; address++ )
+{
+// The i2c_scanner uses the return value of
+// the Write.endTransmisstion to see if
+// a device did acknowledge to the address.
+Wire.beginTransmission(address);
+error = Wire.endTransmission();
+if (error == 0)
+{
+Serial.print("I2C device found at address 0x");
+if (address<16)
+Serial.print("0");
+Serial.print(address,HEX);
+Serial.println(" !");
+nDevices++;
+}
+else if (error==4)
+{
+Serial.print("Unknown error at address 0x");
+if (address<16)
+Serial.print("0");
+Serial.println(address,HEX);
+}
+}
+if (nDevices == 0)
+Serial.println("No I2C devices found\n");
+else
+Serial.println("done\n");
+delay(3000);
+// wait 5 seconds for next scan
 }
